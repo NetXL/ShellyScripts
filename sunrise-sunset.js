69e7160d745f3d2dd14267c905748169068df279
Geoff Wilson <gw@netxl.com>
2026-01-20 12:27:24 +0000

    Add a retry to the sunrise API. Allow ON/OFF to be inverted. Use print instead of console.log.

--- sunrise-sunset.js
+++ sunrise-sunset.js
@@ -1,11 +1,14 @@
+/**
+ *
+ */
+
 const CONFIG = {
-    debug: false,
+    debug: false, // logs messages to the console when set to true
+    inverse: false, // set to true to turn the Plug OFF at the ON time (e.g. sunrise) and ON at the OFF time (sunset)
     location: {
-        name: 'Guildford', // City (for future auto Geolocation lat/lng)
-        cc: 'GB', // Country Code (for future auto Geolocation lat/lng)
         latitude: '51.2362', // Latitude of location
         longitude: '-0.5704', // Longitude of location
-        timeZone: 'Europe/London' // Time Zone of location (TODO - Check what TZ the Shelly uses)
+        timeZone: 'Europe/London' // Time Zone of location
     },
     weekday: {
         on: {
@@ -42,6 +45,7 @@ const CONFIG = {
 }
 
 let apiResponse;
+let oneHour = 60 * 60 * 1000; // 1 hour in milliseconds
 
 function refreshSunriseData(forDay, callback) {
     log('Refreshing sunrise data for ' + forDay)
@@ -51,7 +55,13 @@ function refreshSunriseData(forDay, callback) {
             url: 'https://api.sunrise-sunset.org/json?lat=' + location.latitude + '&lng=' + location.longitude + '&formatted=0&tzid=' + location.timeZone + '&date=' + forDay
         },
         function (result, errorCode) {
-            if (errorCode > 0) return;
+            if (errorCode !== 0) {
+                log('Unable to fetch latest sunrise data, retrying in 1 hour');
+                Timer.set(oneHour, false, function () {
+                    refreshSunriseData(forDay, callback);
+                });
+                return;
+            }
             apiResponse = JSON.parse(result.body).results
             callback();
         }
@@ -60,7 +70,8 @@ function refreshSunriseData(forDay, callback) {
 
 function configureNextTimer(turnOn, targetTime) {
     Timer.set(getTimePeriod(targetTime), false, function () {
-        Shelly.call("Switch.set", {'id': 0, 'on': turnOn}, calculateNextTimer);
+        const on = CONFIG.inverse ? !turnOn : turnOn;
+        Shelly.call("Switch.Set", {'id': 0, 'on':  on}, calculateNextTimer);
     })
 }
 
@@ -90,15 +101,15 @@ function calculateNextTimer() {
 
     if (currentTime < onTime) {
         // We're setting the timer to turn on this morning
-        log('we`re before the on time, setting timer to turn on the plug in the morning')
+        log('Before the on time, setting timer to turn on the plug in the morning')
         configureNextTimer(true, onTime)
     } else if (currentTime < offTime) {
         // We're setting the time to turn off today
-        log('we`re after on time but before off time, setting timer to turn off the plug in the evening')
+        log('After on time but before off time, setting timer to turn off the plug in the evening')
         configureNextTimer(false, offTime)
     }  else {
-        // We're setting the timer to turn on tomorrow morning
-        log('we`re after the on and off times, setting timer to turn the plug on tomorrow morning')
+        // No timer for today, we need to update the data for tomorrow
+        log('After both the on and off times, getting data for tomorrow')
         refreshSunriseData('tomorrow', calculateNextTimer);
     }
 
@@ -123,7 +134,7 @@ function setup() {
 
 function log(message) {
     if (!CONFIG.debug) return;
-    console.log(message);
+    print(message);
 }
 
 setup();
